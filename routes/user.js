7e8b464a478c62a1d506a0c35759b6da41e7bdfe
Felipe <felipe.16costa@gmail.com>
2020-08-07 15:28:51 -0300

    change

--- routes/user.js
+++ routes/user.js
@@ -18,10 +18,25 @@ router.post('/user', async (req, res) => {
         const token = await user.generateAuthToken();
         res.header('auth', token).send(user);
     } catch (e) {
-        res.status(400).send(e);
+        res.status(400).send({ message: handleError(e.message, req) });
     }
 
 });
+
+let handleError = (message, req) => {
+
+    if (message.includes(req.body.email) && req.body.email)
+        return 'Email already exists'
+
+    if (message.includes('name'))
+        return 'Must provide a valid name'
+
+    if (message.includes('password'))
+        return 'Must provide a valid password'
+
+    if (message.includes('email'))
+        return 'Must provide a valid email'
+}
 
 // All users
 router.get('/users', async (req, res) => {
@@ -54,20 +69,22 @@ router.get('/user/me', (req, res) => {
 });
 
 //Login user
-router.post('/user/login', async(req, res) => {
+router.post('/user/login', async (req, res) => {
     try {
         const body = _.pick(req.body, ['email', 'password']);
         const user = await User.findByCredentials(body.email, body.password);
         const token = await user.generateAuthToken();
-        res.header('auth', token).send({token, user});
+        res.header('auth', token).send({ token, user });
     } catch (e) {
-        res.status(400).send({error: e});
+        res.status(400).send({
+            message: 'Email or password is incorrect'
+        });
     }
 
 });
 
 //Logout removing token passed 
-router.delete('/user/me/token', async(req, res) => {
+router.delete('/user/me/token', async (req, res) => {
     try {
         await req.user.removeToken(req.token);
         res.status(200).send();
