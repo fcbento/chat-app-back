2b31aee7def4975bbd4feec66a70bc7eafc34358
Felipe <felipe.16costa@gmail.com>
2020-08-07 16:58:06 -0300

    USer

--- socket.js
+++ socket.js
@@ -46,7 +46,7 @@ const socketServer = (io, server) => {
         socket.on('createMessage', (message, callback) => {
             var user = users.getUser(socket.id);
             if (user && isRealString(message.text)) {
-                io.to(user.room).emit('newMessage', generateMessage(user.user.name, message.text));
+                io.to(user.room).emit('newMessage', generateMessage(user, message.text));
             }
             callback();
         });
