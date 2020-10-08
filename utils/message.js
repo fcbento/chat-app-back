0fc1a4e683763d47139cc2c8b98650d499061bbb
Felie Bento <felipe.16costa@gmail.com>
2020-10-08 11:41:56 -0300

    Youtube / Audio message

--- utils/message.js
+++ utils/message.js
@@ -1,11 +1,12 @@
 const moment = require('moment');
 
-const generateMessage = (from, text, isYoutube) => {
+const generateMessage = (from, text, isYoutube, isAudio) => {
     return {
         from,
         text,
         createdAt: moment().valueOf(),
-        isYoutube
+        isYoutube,
+        isAudio
     };
 };
 
